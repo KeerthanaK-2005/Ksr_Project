2953875c244869fe3ba6dbadf9194817b022683c
KeerthanaK-2005 <keerthanacse2023@ksrce.ac.in>
2025-08-07 15:37:56 +0530

    Initial commit
    Generated by react-native@0.80.2

---
+++ android/app/src/main/java/com/vivaah_0408/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.vivaah_0408
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "Vivaah_0408"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
